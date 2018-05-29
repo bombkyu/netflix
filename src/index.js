db8b60f4746367dc8a641b00bf0c649ca8532a81
Bumkyu <bombkyu@gmail.com>
2018-05-29 14:06:20 +0900

    Create Video Controller CSS

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const header = document.querySelector(".js-header"),
     video = document.querySelector(".js-video"),
     muteBtn = document.querySelector(".js-muteBtn"),
     playBtn = document.querySelector(".js-playBtn"),
+    volumeController = document.querySelector(".js-volumeController"),
     volumeRange = document.querySelector(".js-volumeRange"),
     search = document.querySelector(".js-header__others-search"),
     searchInput = document.querySelector(".js-search"),
@@ -12,22 +13,20 @@ const header = document.querySelector(".js-header"),
 // video.autoplay = true;
 
 const loadSettings = () => {
+    
     const loadMute = localStorage.getItem("muted");
     volumeRange.value = video.volume;
+
     // Returned loadMute is not a boolean, it is a string!!
     if(loadMute === "true") {
-        video.muted = true;
-        volumeRange.value = 0;
-        muteBtn.innerHTML="Unmute";
+        muteSound();
     } else {
-        video.muted = false;
-        
-        muteBtn.innerHTML="mute";
+        unmuteSound();
     }
 }
 
 video.onended = () => {
-    playBtn.innerHTML="Play";
+    pauseVideo();
 }
 
 const handleScroll = event => {
@@ -38,61 +37,54 @@ const handleScroll = event => {
     } else {
         header.classList.remove("black");
     }
-
-    if (scrollHeight > 500) {
-        video.pause();
-        playBtn.innerHTML = "Play";
+    
+    if (scrollHeight > 300) {
+        pauseVideo();
         
     } else {
-        video.play();
-        video.onplay = () => {
-            playBtn.innerHTML = 'Pause';
-        }
+        playVideo();
     }
+    
 };
 
 const handleMute = () => {
     const isMute = video.muted;
     if (isMute) {
         // If is muted, change it to unmuted 
-        video.muted = false;
-        muteBtn.innerHTML="mute";
+        unmuteSound();
         volumeRange.value = video.volume;
         localStorage.setItem("muted", false);
     } else {
         // If is not muted, change it to muted
-        
-        video.muted = true;
-        muteBtn.innerHTML="Unmute";
-        volumeRange.value = 0;
+        muteSound();
         localStorage.setItem('muted', true);
     }
 };
 
 const handlePlay = () => {
     const isPaused = video.paused;
-    // console.log(isPaused);
     if(isPaused) {
         // Play the video, and change the play button text to pause
-        video.play();
-        playBtn.innerHTML = "Pause";
+        playVideo();
         
     } else {
         // Pause the video, and change the pause button text to play
-        video.pause();
-        playBtn.innerHTML="Play";
+        pauseVideo();
     }
 }
 
 const handleVolume = event => {
     video.volume = event.target.value;
-    // console.log(event.target.value);
+    if(video.volume === 0) {
+        muteSound();
+    } else {
+        unmuteSound();
+    }
 }
 
 const handleSearch = event => {
     console.log(event.target);
     const searchClass = event.target.className;
-    console.log(searchClass);
     if(searchClass.includes("fa-search")) {
         search.innerHTML = `
         <form class="js-search-form">
@@ -106,16 +98,48 @@ const handleSearch = event => {
     
 }
 const handleSearchInput = event => {
-    console.log(searchInput.value);
+
 }
 const handleSubmit = event => {
-    console.log("hello!");
     event.preventDefault();
 }
+
+const handleMouseEnter = event => {
+    volumeRange.style.display="block";
+}
+
+const handleMouseLeave = event => {
+    volumeRange.style.display = "none";
+}
+
+const playVideo = event => {
+    video.play();
+    if(!video.paused) {
+        playBtn.innerHTML = `<i class="fa fa-pause"></i>`;
+    }
+}
+
+const pauseVideo = event => {
+    video.pause();
+    playBtn.innerHTML = `<i class="fa fa-play"></i>`;
+}
+
+const muteSound = event => {
+    video.muted = true;
+    muteBtn.innerHTML = `<i class="fa fa-volume-off fa-lg"></i>`;
+    volumeRange.value = 0;
+}
+const unmuteSound = event => {
+    video.muted = false;
+    muteBtn.innerHTML = `<i class="fa fa-volume-up fa-lg"></i>`;
+};
+
 
 loadSettings();
 
 muteBtn.addEventListener("click", handleMute);
+muteBtn.addEventListener('mouseenter', handleMouseEnter);
+volumeController.addEventListener('mouseleave', handleMouseLeave);
 playBtn.addEventListener("click", handlePlay);
 volumeRange.addEventListener("change", handleVolume);
 search.addEventListener("click", handleSearch);
