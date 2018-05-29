6eb17d156369b21247a1b140b9304bb8c8a12d10
Bumkyu <bombkyu@gmail.com>
2018-05-29 09:53:52 +0900

    Create Webpack Development Config

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -2,11 +2,34 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    mode:"development",
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'style-loader',
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1,
+						},
+					},
+					{
+						loader: 'postcss-loader',
+					},
+				],
+			},
+		],
+	},
 	devServer: {
 		contentBase: path.join(__dirname, '../src'),
 		publicPath: '/',
 		progress: true,
 		port: 8080,
+		hot: true,
 	},
+	plugins: [new webpack.HotModuleReplacementPlugin()],
 };
